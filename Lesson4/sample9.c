8f54adac3929c96739a3c1c64a1984f88df228bc
Soutenn-AO <a.watanabe.dendai@gmail.com>
2026-07-01 13:30:01 +0900

    feat: add Lesson4 sample9

---
+++ Lesson4/sample9.c
@@ -0,0 +1,20 @@
+// 小さなサイズの型に変換する
+
+#include <stdio.h>
+
+int main(void){
+	double dnum;
+	int inum;
+
+	dnum = 160.5;
+
+	printf("身長は%fセンチです。\n", dnum);
+
+	printf("int型の変数に代入します。\n");
+
+	inum = dnum;
+
+	printf("身長は%dセンチです。\n", inum);
+
+	return 0;
+}
