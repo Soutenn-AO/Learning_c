8087ffd2f5c972ff9932c8a0f134952b3c2cf9fe
Soutenn-AO <a.watanabe.dendai@gmail.com>
2026-07-01 12:30:44 +0900

    feat: add Lesson4 sample4

---
+++ Lesson4/sample4.c
@@ -0,0 +1,17 @@
+// いろいろな演算子を利用する
+
+#include <stdio.h>
+
+int main(void){
+	int num1 = 10;
+	int num2 = 5;
+
+	printf("num1とnum2にいろいろな演算を行います。\n");
+	printf("num1+num2は%dです。\n", num1+num2);
+	printf("num1-num2は%dです。\n", num1-num2);
+	printf("num1*num2は%dです。\n", num1*num2);
+	printf("num1/num2は%dです。\n", num1/num2);
+	printf("num1%%num2は%dです。\n", num1%num2);
+
+	return 0;
+}
