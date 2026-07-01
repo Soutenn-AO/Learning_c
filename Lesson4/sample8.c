dd62a4b37396f15eb9aea97a43fe0585db493b27
Soutenn-AO <a.watanabe.dendai@gmail.com>
2026-07-01 13:24:15 +0900

    feat: add Lesson4 sample8

---
+++ Lesson4/sample8.c
@@ -0,0 +1,20 @@
+// 大きなサイズの型に代入する
+
+#include <stdio.h>
+
+int main(void){
+	int inum;
+	double dnum;
+
+	inum = 160;
+
+	printf("身長は%dセンチです。\n", inum);
+
+	printf("double型の変数に代入します。\n");
+
+	dnum = inum;
+
+	printf("身長は%fセンチです。\n", dnum);
+
+	return 0;
+}
