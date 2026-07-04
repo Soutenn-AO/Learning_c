f315a6fb049085dd4abee314616b732ee6505bbd
Soutenn-AO <a.watanabe.dendai@gmail.com>
2026-07-04 12:44:59 +0900

    feat: add Lesson6 sample9

---
+++ Lesson6/sample9.c
@@ -0,0 +1,26 @@
+// if文などと組み合わせる
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j, ch;
+	ch = 0;
+
+	for(i=0; i<5; i++){
+		for(j=0; j<5; j++){
+			if(ch == 0){
+				printf("*");
+				ch = 1;
+			}
+			else{
+				printf("_");
+				ch = 0;
+			}
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+
