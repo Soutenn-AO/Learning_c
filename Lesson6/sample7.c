b990a4d2cd4bff4769975e03a24c65b1c01b7438
Soutenn-AO <a.watanabe.dendai@gmail.com>
2026-07-04 12:30:36 +0900

    feat: add Lesson6 sample7

---
+++ Lesson6/sample7.c
@@ -0,0 +1,17 @@
+// do~while文を使う
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 1;
+
+	do{
+		printf("%d番目の繰り返しです。\n", i);
+		i++;
+	}while(i<=5);
+
+	printf("繰り返しが終わりました。\n");
+
+	return 0;
+}
