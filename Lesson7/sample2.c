0bb299f3e7aaf90c56daed79a126439c54006790
Soutenn-AO <a.watanabe.dendai@gmail.com>
2026-07-05 22:32:34 +0900

    feat: add Lesson7 sample2

---
+++ Lesson7/sample2.c
@@ -0,0 +1,21 @@
+// キーボードから入力する
+
+#include <stdio.h>
+
+int main(void)
+{
+	int test[5];
+	int i, j;
+
+	printf("5人の点数を入力してください。\n");
+	for(i=0; i<5; i++){
+		printf("%d人目：", i+1);
+		scanf("%d", &test[i]);
+	}
+
+	for(j=0; j<5; j++){
+		printf("%d番目の人の点数は%dです。\n", j+1, test[j]);
+	}
+
+	return 0;
+}
