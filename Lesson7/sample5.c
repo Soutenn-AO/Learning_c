65b18ea99b5175a2b68421e9cc35dcd976edda88
Soutenn-AO <a.watanabe.dendai@gmail.com>
2026-07-07 22:12:52 +0900

    feat: add Lesson7 sample5

---
+++ Lesson7/sample5.c
@@ -0,0 +1,34 @@
+// 配列をソートする
+
+#include <stdio.h>
+#define NUM 5
+
+int main(void)
+{
+	int test[NUM];
+	int tmp;
+	int i, j, s, t;
+
+	printf("%d人の点数を入力してください。\n", NUM);
+	for(i=0; i<NUM; i++){
+		printf("%d人目：", i+1);
+		scanf("%d", &test[i]);
+	}
+
+	// 配列をソートする
+	for(s=0; s<NUM-1; s++){
+		for(t=s+1; t<NUM; t++){
+			if(test[t] > test[s]){
+				tmp = test[t];
+				test[t] = test[s];
+				test[s] = tmp;
+			}
+		}
+	}
+
+	for(j=0; j<NUM; j++){
+		printf("%d番目の人の点数は%dです。\n", j+1, test[j]);
+	}
+
+	return 0;
+}	
