e2753ec654fe5367f760bba8641dc9a5b7b0821a
Soutenn-AO <a.watanabe.dendai@gmail.com>
2026-07-27 14:19:06 +0900

    feat: add Lesson8 sample4

---
+++ Lesson8/sample4.c
@@ -0,0 +1,24 @@
+// 実引数を変数の値とする
+
+#include <stdio.h>
+
+// buy関数の定義
+void buy(int x){
+	printf("%d万円の車を買いました。\n", x);
+}
+
+// buy関数の呼び出し
+int main(void)
+{
+	int num;
+
+	printf("1台目はいくらの車を買いますか？\n");
+	scanf("%d", &num);
+	buy(num);
+
+	printf("2台目はいくらの車を買いますか？\n");
+	scanf("%d", &num);
+	buy(num);
+
+	return 0;
+}
